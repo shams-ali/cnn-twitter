60986e49e9ac8600cdb62ea251bca80795d8537d
Shams Ali <shams.ali0216@gmail.com>
2017-11-09 21:45:36 -0600

    (initialize) twitter api

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 require("dotenv").config();
+const {
+  PORT,
+  consumer_key,
+  consumer_secret,
+  access_token_key,
+  access_token_secret
+} = process.env;
 
-const express = require("express");
 const path = require("path");
-
+const express = require("express");
+const Twitter = require("twitter");
 const app = express();
-
-const PORT = process.env.PORT || 3000;
+const Promise = require("bluebird");
 
 app.use(express.static(path.join(__dirname, "build")));
+
+const twitterClient = new Twitter({
+  consumer_key,
+  consumer_secret,
+  access_token_key,
+  access_token_secret
+});
+Promise.promisifyAll(twitterClient);
+
+app.get("/search", ({ query: { q } }, res) =>
+  twitterClient
+    .getAsync("search/tweets", { q, count: 10 })
+    .then(data => res.send(data))
+    .catch(err => res.send(err))
+);
 
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "build/index.html"))
--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -12,7 +12,7 @@ const App = () => (
 
     <main>
       <Route exact path="/" component={Home} />
-      {<Route exact path="/about-us" component={About} />}
+      <Route exact path="/about-us" component={About} />
     </main>
   </div>
 );
