fdfac4674b81f436c2442f279680dafc00516b4e
Shams Ali <shams.ali0216@gmail.com>
2017-11-10 00:15:51 -0600

    (initialize) flow

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,31 +1,29 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
 
-const SearchBar = ({ handleSubmit, pristine, reset, submitting }) => {
-  return (
-    <form onSubmit={handleSubmit}>
+const SearchBar = ({ handleSubmit, pristine, reset, submitting }) => (
+  <form onSubmit={handleSubmit}>
+    <div>
+      <label>Search</label>
       <div>
-        <label>Search</label>
-        <div>
-          <Field
-            name="search"
-            component="input"
-            type="text"
-            placeholder="Search Twitter!!"
-          />
-        </div>
+        <Field
+          name="search"
+          component="input"
+          type="text"
+          placeholder="Search Twitter!!"
+        />
       </div>
-      <div>
-        <button type="submit" disabled={pristine || submitting}>
-          Submit
-        </button>
-        <button type="button" disabled={pristine || submitting} onClick={reset}>
-          Clear Values
-        </button>
-      </div>
-    </form>
-  );
-};
+    </div>
+    <div>
+      <button type="submit" disabled={pristine || submitting}>
+        Submit
+      </button>
+      <button type="button" disabled={pristine || submitting} onClick={reset}>
+        Clear Values
+      </button>
+    </div>
+  </form>
+);
 
 export default reduxForm({
   form: "searchBar"
